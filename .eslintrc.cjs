bdcdb21717fa2b132127d0699c866f7a01255817
zengbo0710 <zengbo1228@gmail.com>
2026-04-09 12:11:10 +0800

    chore: initialise monorepo root (npm workspaces, TS, ESLint, Prettier)

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
