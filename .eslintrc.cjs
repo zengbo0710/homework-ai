b06a842df5ce4ff7cd96abe968e119020b28dbcc
zengbo0710 <zengbo1228@gmail.com>
2026-04-09 12:13:40 +0800

    fix(root): add ESLint parserOptions, eslint-config-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,18 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
   plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   settings: {
     react: { version: 'detect' },
